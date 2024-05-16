d8188efeb3f024005e39c4750e09170f9ff3a538
sofy <bedlusofi@gmail.com>
2024-05-16 09:35:09 +0300

    This commit adds error handling to the `handleProfileSignup` function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -13,5 +13,6 @@ export default function handleProfileSignup() {
         }
       }
       console.log(values.join(' '));
-    });
+    })
+    .catch(() => console.log('Signup system offline'));
 }
