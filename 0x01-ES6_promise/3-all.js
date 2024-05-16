e2ab42458bb2e20e8c5d7668ce5c5d06d3c75a1c
sofy <bedlusofi@gmail.com>
2024-05-16 10:42:22 +0300

    refactor: Improve error handling in handleProfileSignup function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,20 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-    try {
-        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-        const values = [];
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    const values = [];
 
-        if ('body' in photo) {
-            values.push(photo.body);
-        }
-
-        if ('firstName' in user && 'lastName' in user) {
-            values.push(user.firstName, user.lastName);
-        }
-
-        console.log(values.join(' '));
-    } catch {
-        console.log('Signup system offline');
+    if ('body' in photo) {
+      values.push(photo.body);
     }
+
+    if ('firstName' in user && 'lastName' in user) {
+      values.push(user.firstName, user.lastName);
+    }
+
+    console.log(values.join(' '));
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
