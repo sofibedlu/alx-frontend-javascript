9979489b3de47f6d8e865e65560434c788f18042
sofy <bedlusofi@gmail.com>
2024-05-24 15:52:14 +0300

    feat: Add Pricing class
    This commit adds the `Pricing` class to the `0x02-ES6_classes/4-pricing.js` file. The class has a constructor that takes in an `amount` and `currency` parameter and assigns them to the `_amount` and `_currency` properties respectively. It includes getter and setter methods for `amount` and `currency`, as well as a `displayFullPrice` method that returns a formatted string with the amount and the currency's full display. It also includes a static method `convertPrice` that takes in an `amount` and `conversionRate` and returns the converted price. This class is now ready to be used for pricing calculations and display.

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
