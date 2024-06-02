1dfee13738024368cb7bd2c8afced090e59c1869
sofy <bedlusofi@gmail.com>
2024-06-02 17:56:01 +0300

    feat: Add Teacher interface for TypeScript task

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,17 @@
+interface Teacher {
+    // firstName and lastName can only be assigned when the object is first initialized
+    readonly firstName: string;
+    readonly lastName: string;
+
+    // fullTimeEmployee is always defined
+    fullTimeEmployee: boolean;
+
+    // yearsOfExperience is optional
+    yearsOfExperience?: number;
+
+    // location is always defined
+    location: string;
+
+    // This interface can have any other property with a string name
+    [propName: string]: any;
+}
