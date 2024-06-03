61bc8cdda85582864304356337c09b3f3f434294
sofy <bedlusofi@gmail.com>
2024-06-03 11:26:52 +0300

    task-2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,46 @@
+// Define the DirectorInterface
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// Define the TeacherInterface
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+    workFromHome() {
+        return 'Working from home';
+    }
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+
+export class Teacher implements TeacherInterface {
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+    workTeacherTasks() {
+        return 'Getting to work';
+    }
+}
+
+
+export function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
