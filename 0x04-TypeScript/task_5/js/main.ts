af1c4920f6634df5c15232984997cab259f92697
sofy <bedlusofi@gmail.com>
2024-06-04 15:24:51 +0300

    feat: Add TypeScript task 5 files and dependencies

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+interface MajorCredits {
+  credits: number;
+  _brand?: 'major';
+}
+
+interface MinorCredits {
+  credits: number;
+  _brand?: 'minor'; 
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits, _brand: 'major' };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits, _brand: 'minor' };
+}
